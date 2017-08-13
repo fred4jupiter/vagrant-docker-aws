4209b8af5ac60c4df979c5cc1f6f4d3f3bbe3197
Staehler, Michael <michael.staehler@opitz-consulting.com>
2017-08-13 13:53:43 +0200

    update install scripts

--- scripts/install_aws-cli.sh
+++ scripts/install_aws-cli.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # Install AWS CLI
+sudo apt-get update
+sudo apt install python-pip -y 
+sudo pip install --upgrade pip
 sudo apt install awscli -y
+sudo pip install --upgrade --user awscli
--- scripts/install_aws-ecs-cli.sh
+++ scripts/install_aws-ecs-cli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Install AWS ECS CLI
-sudo apt-get update -y
+sudo apt-get update
 sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
 sudo chmod +x /usr/local/bin/ecs-cli
